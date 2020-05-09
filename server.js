6a34676c152e8a87ba21b20fa5ee9fafbe537066
gm_desktop <gmesda23@gmail.com>
2020-05-09 15:33:20 -0400

    heroku test 4

--- server.js
+++ server.js
@@ -41,12 +41,12 @@ app.use((req, res, next) => {
 
 
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // app.use(express.static('public'))
